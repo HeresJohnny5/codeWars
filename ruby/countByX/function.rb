db5a5f3aa61754d0a8205523faa4eacb6cc9b602
John Erickson <ericksonjohn08@hotmail.com>
2017-01-30 21:33:12 -0500

    Add different approach to kata using a range with each iteration. Add 2 new tests to test.rb.

--- ruby/countByX/function.rb
+++ ruby/countByX/function.rb
@@ -2,11 +2,23 @@
   Create a function with two arguments that will return a list of length (n) with multiples of (x). Assume both the given number and the number of times to count will be positive numbers greater than 0. Return the results as an array (or list in Python, Haskell or Elixir).
 =end
 
-def count_by_x_upto(x, n)
+#upto method
+def count_by_x_upto(multiples, num_length)
   new_array = []
 
-  1.upto(n) do |num|
-    new_array << (num * x)
+  1.upto(num_length) do |num|
+    new_array << (num * multiples)
+  end
+
+  new_array
+end
+
+#range/each method
+def count_by_x_range(multiples, num_length)
+  new_array = Array.new
+
+  (1..num_length).each do |num|
+    new_array.push(num * multiples)
   end
 
   new_array
--- ruby/countByX/test.rb
+++ ruby/countByX/test.rb
@@ -4,13 +4,25 @@ require_relative 'function.rb'
 class TestCountByX < Minitest::Test
   def test_returns_array_2_4_6
     num_length = 3
-    multiples_of_length = 2
-    assert_equal([2, 4, 6], count_by_x_upto(multiples_of_length, num_length))
+    multiples = 2
+    assert_equal([2, 4, 6], count_by_x_upto(multiples, num_length))
   end
 
   def test_returns_array_2_4_6_8
     num_length = 4
-    multiples_of_length = 2
-    assert_equal([2, 4, 6, 8], count_by_x_upto(multiples_of_length, num_length))
+    multiples = 2
+    assert_equal([2, 4, 6, 8], count_by_x_upto(multiples, num_length))
+  end
+
+  def test_returns_array_3_6_9
+    num_length = 3
+    multiples = 3
+    assert_equal([3, 6, 9], count_by_x_range(multiples, num_length))
+  end
+
+  def test_returns_array_3_6_7_12
+    num_length = 4
+    multiples = 3
+    assert_equal([3, 6, 9, 12], count_by_x_range(multiples, num_length))
   end
 end
