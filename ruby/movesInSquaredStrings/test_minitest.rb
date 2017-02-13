c5ff38f12cc21d91cd7389dc14a7d6e298c02de0
John Erickson <ericksonjohn08@hotmail.com>
2017-02-12 20:43:35 -0500

    Remove string variable to test_string_returns_reversed_string. Add 2 new tests.

--- ruby/movesInSquaredStrings/test_minitest.rb
+++ ruby/movesInSquaredStrings/test_minitest.rb
@@ -3,8 +3,9 @@ require 'minitest/autorun'
 
 class MovesInSquaredStrings < Minitest::Test
   def test_string_returns_reversed_string
-    string = "abcd\nefgh\nijkl\nmnop"
     sequence = SequenceOfSquaredStrings.new
-    assert_equal("dcba\nhgfe\nlkji\nponm", sequence.vert_mirror(string))
+    assert_equal("dcba\nhgfe\nlkji\nponm", sequence.vert_mirror("abcd\nefgh\nijkl\nmnop"))
+    assert_equal("hSgdHQ\nHnDMao\nClNNxX\niRvxxH", sequence.vert_mirror("QHdgSh\noaMDnH\nXxNNlC\nHxxvRi"))
+    assert_equal("IzOTWE\nkkbeCM\nWuzZxM\nvDddJw", sequence.vert_mirror("EWTOzI\nMCebkk\nMxZzuW\nwJddDv"))
   end
 end
