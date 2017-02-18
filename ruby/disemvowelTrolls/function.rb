0d3f9002648086e537a75471acb573fd93072836
John Erickson <ericksonjohn08@hotmail.com>
2017-02-18 10:50:44 -0500

    Add StopTrolling class with initialize and disemvowel method. One test passing

--- ruby/disemvowelTrolls/function.rb
+++ ruby/disemvowelTrolls/function.rb
@@ -1,3 +1,11 @@
-def disemvowel()
-  true
+class StopTrolling
+  attr_accessor :statement
+
+  def initialize(statement)
+    @statement = statement
+  end
+
+  def disemvowel()
+    @statement.delete("aeiouAEIOU")
+  end
 end
--- ruby/disemvowelTrolls/test.rb
+++ ruby/disemvowelTrolls/test.rb
@@ -2,7 +2,9 @@ require_relative 'function.rb'
 require 'minitest/autorun'
 
 class TestDisemvowelTrolls < Minitest::Test
-  def test_connectivity
-    assert_equal(true, disemvowel())
+  def test_string_removes_vowels_regardless_of_case
+    statement = "This website is for losers LOL!"
+    stop_trolling = StopTrolling.new(statement)
+    assert_equal("Ths wbst s fr lsrs LL!", stop_trolling.disemvowel())
   end
 end
