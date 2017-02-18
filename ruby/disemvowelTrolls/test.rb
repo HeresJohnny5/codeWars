a1a02edee8b545a56e20138e4e111e527539912b
John Erickson <ericksonjohn08@hotmail.com>
2017-02-18 10:54:22 -0500

    Add 2 new tests to test.rb

--- ruby/disemvowelTrolls/test.rb
+++ ruby/disemvowelTrolls/test.rb
@@ -7,4 +7,16 @@ class TestDisemvowelTrolls < Minitest::Test
     stop_trolling = StopTrolling.new(statement)
     assert_equal("Ths wbst s fr lsrs LL!", stop_trolling.disemvowel())
   end
+
+  def test_string_removes_vowels_regardless_of_case
+    statement = "You smell like cheese and onion!"
+    stop_trolling = StopTrolling.new(statement)
+    assert_equal("Y smll lk chs nd nn!", stop_trolling.disemvowel())
+  end
+
+  def test_string_removes_vowels_regardless_of_case
+    statement = "How can you be such an idiot?"
+    stop_trolling = StopTrolling.new(statement)
+    assert_equal("Hw cn y b sch n dt?", stop_trolling.disemvowel())
+  end
 end
