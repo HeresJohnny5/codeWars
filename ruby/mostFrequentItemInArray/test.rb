eb869c8de7085db946e4bb188200d5d9db64fd66
John Erickson <ericksonjohn08@hotmail.com>
2017-02-18 09:50:30 -0500

    Add 2 more tests to test.rb

--- ruby/mostFrequentItemInArray/test.rb
+++ ruby/mostFrequentItemInArray/test.rb
@@ -2,8 +2,10 @@ require 'minitest/autorun'
 require_relative 'function.rb'
 
 class TestMostFrequentItemCount < Minitest::Test
-  def test_connectivity
+  def test_responds_to_array_returning_integer_of_most_frequent_item
     code_wars_kata = CodeWarsKata.new
     assert_equal(5, code_wars_kata.most_frequent_item_count([3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3]))
+    assert_equal(2, code_wars_kata.most_frequent_item_count([3, -1, -1]))
+    assert_equal(1, code_wars_kata.most_frequent_item_count([9]))
   end
 end
