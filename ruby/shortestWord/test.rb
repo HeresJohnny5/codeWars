f616895348f936a9c5c1248d30f029e7d88ace72
John Erickson <ericksonjohn08@hotmail.com>
2017-03-03 23:58:56 -0500

    Add additional test

--- ruby/shortestWord/test.rb
+++ ruby/shortestWord/test.rb
@@ -2,13 +2,18 @@ require 'minitest/autorun'
 require_relative 'function.rb'
 
 class TestShortestWord < Minitest::Test
-  def test_statement_short_word_returns_count_of_3
-    statement = "bitcoin take over the world maybe who knows perhaps"
-    assert_equal(3, shortest_word(statement))
-  end
-
   def test_statement_short_word_returns_count_of_1
     statement = "i want to travel the world writing code one day"
     assert_equal(1, shortest_word(statement))
+  end
+
+  def test_statement_short_word_returns_count_of_2
+    statement = "Lets all go on holiday somewhere very cold"
+    assert_equal(2, shortest_word(statement))
+  end
+
+  def test_statement_short_word_returns_count_of_3
+    statement = "bitcoin take over the world maybe who knows perhaps"
+    assert_equal(3, shortest_word(statement))
   end
 end
