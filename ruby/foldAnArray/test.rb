be5f4c2afec91384e6de598b4882eabaf853b2c5
John Erickson <ericksonjohn08@hotmail.com>
2017-03-05 22:45:16 -0500

    Add additional tests to test.rb

--- ruby/foldAnArray/test.rb
+++ ruby/foldAnArray/test.rb
@@ -13,4 +13,22 @@ class TestFoldAnArray < Minitest::Test
     fold_iteration = 2
     assert_equal([9, 6], fold_array(original_array, fold_iteration))
   end
+
+  def test_array_folds_1_time_returning_array_9_6
+    original_array = [6, 6, 3]
+    fold_iteration = 1
+    assert_equal([9, 6], fold_array(original_array, fold_iteration))
+  end
+
+  def test_array_folds_3_time_returning_array_15
+    original_array = [15]
+    fold_iteration = 1
+    assert_equal([15], fold_array(original_array, fold_iteration))
+  end
+
+  def test_array_folds_1_time_returning_array_14_75_0
+    original_array = [-9, 9, -8, 8, 66, 23]
+    fold_iteration = 1
+    assert_equal([14, 75, 0], fold_array(original_array, fold_iteration))
+  end
 end
