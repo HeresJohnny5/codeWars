2b2299627a53eaebb4db4e89465ace1c41e58de1
John Erickson <ericksonjohn08@hotmail.com>
2017-04-22 13:35:47 -0400

    Add for loop, looping through array and conditional

--- javaScript/firstNonConsecutiveNumber/app.js
+++ javaScript/firstNonConsecutiveNumber/app.js
@@ -1,6 +1,16 @@
 function firstNonConsecutive(arr) {
+	var allConsecutive = null;
 	
+	for(var i = 0; i < arr.length - 1; i++) {
+		if(arr[i + 1] - arr[i] !== 1) {
+			return arr[i + 1];
+		}
+	}
+	return allConsecutive;
 }
 
+var arr = [1, 2, 3, 4, 6, 7, 8];
+console.log(firstNonConsecutive(arr));
+
 var arr = [1, 2, 3, 4, 5, 6, 7, 8];
-firstNonConsecutive(arr);
+console.log(firstNonConsecutive(arr));
